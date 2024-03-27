b2c246e580d2441d1ea314852fdec5ed9793e4e4
Jip Rietveld <jip_rietveld@hotmail.com>
2024-03-27 13:30:04 +0100

    [ADD] Tests for calculating capacity and cycles accounting for RTE

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -119,6 +119,175 @@ class TestBenchmark(unittest.TestCase):
         optimal_cycles = [cycles[i].solution_value() for i in range(0, len(cycles))]
         self.assertEqual(1.5, max(optimal_cycles))  # Default allowed_cycles
 
+    def test_cycles_are_influenced_by_lower_rte(self):
+        solver_high_cycles = pywraplp.Solver('TEST SOLVER', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+        charge_power_high, discharge_power_high = add_power_schedules_to_solver(
+            solver=solver_high_cycles,
+            schedule_length=20,
+            max_power_kw=10,
+        )
+        capacity_high, cycles_high = add_capacity_and_cycles_to_solver(
+            solver=solver_high_cycles,
+            charge_power=charge_power_high,
+            discharge_power=discharge_power_high,
+            allowed_cycles=20.0
+        )
+        sum_cycles_high = sum(cycles_high)
+        solver_high_cycles.Maximize(sum_cycles_high)
+        solver_high_cycles.Solve()
+
+        optimal_cycles_high = [cycles_high[i].solution_value() for i in range(0, len(cycles_high))]
+
+        solver_low_cycles = pywraplp.Solver('TEST SOLVER', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+        charge_power_low, discharge_power_low = add_power_schedules_to_solver(
+            solver=solver_low_cycles,
+            schedule_length=20,
+            max_power_kw=10,
+        )
+        capacity_low, cycles_low = add_capacity_and_cycles_to_solver(
+            solver=solver_low_cycles,
+            charge_power=charge_power_low,
+            discharge_power=discharge_power_low,
+            allowed_cycles=20.0,
+            charge_efficiency=0.2,
+            discharge_efficiency=0.2,
+        )
+        sum_cycles_low = sum(cycles_low)
+        solver_low_cycles.Maximize(sum_cycles_low)
+        solver_low_cycles.Solve()
+
+        optimal_cycles_low = [cycles_low[i].solution_value() for i in range(0, len(cycles_low))]
+
+        self.assertGreater(sum(optimal_cycles_high), sum(optimal_cycles_low))
+
+    def test_calculating_capacity_based_on_discharge_power(self):
+        solver = pywraplp.Solver('TEST SOLVER', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+        charge_power, discharge_power = add_power_schedules_to_solver(
+            solver=solver,
+            schedule_length=3,
+            max_power_kw=10,
+        )
+        capacity, cycles = add_capacity_and_cycles_to_solver(
+            solver=solver,
+            charge_power=charge_power,
+            discharge_power=discharge_power,
+            initial_battery_capacity_kwh=20,
+            final_battery_capacity_kwh=0,
+            discharge_efficiency=1.0
+        )
+
+        sum_capacity = sum(capacity)
+        solver.Minimize(sum_capacity)
+        solver.Solve()
+
+        optimal_capacity = [capacity[i].solution_value() for i in range(0, len(capacity))]
+        optimal_discharge_power = [discharge_power[i].solution_value() for i in range(0, len(discharge_power))]
+        self.assertEqual(optimal_capacity[0], 20)
+        self.assertEqual(optimal_discharge_power[0], 10)
+        self.assertEqual(optimal_capacity[1], 10)
+        self.assertEqual(optimal_discharge_power[1], 10)
+        self.assertEqual(optimal_capacity[2], 0)
+        self.assertEqual(optimal_discharge_power[2], 0)
+        self.assertEqual(optimal_capacity[3], 0)
+
+    def test_calculating_capacity_based_on_discharge_power_rte(self):
+        solver = pywraplp.Solver('TEST SOLVER', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+        charge_power, discharge_power = add_power_schedules_to_solver(
+            solver=solver,
+            schedule_length=5,
+            max_power_kw=10,
+        )
+        capacity, cycles = add_capacity_and_cycles_to_solver(
+            solver=solver,
+            charge_power=charge_power,
+            discharge_power=discharge_power,
+            initial_battery_capacity_kwh=20,
+            final_battery_capacity_kwh=0,
+            discharge_efficiency=0.5
+        )
+
+        sum_capacity = sum(capacity)
+        solver.Minimize(sum_capacity)
+        solver.Solve()
+
+        optimal_capacity = [capacity[i].solution_value() for i in range(0, len(capacity))]
+        optimal_discharge_power = [discharge_power[i].solution_value() for i in range(0, len(discharge_power))]
+        self.assertEqual(optimal_capacity[0], 20)
+        self.assertEqual(optimal_discharge_power[0], 10)
+        self.assertAlmostEqual(optimal_capacity[1], 15)
+        self.assertEqual(optimal_discharge_power[1], 10)
+        self.assertAlmostEqual(optimal_capacity[2], 10)
+        self.assertEqual(optimal_discharge_power[2], 10)
+        self.assertAlmostEqual(optimal_capacity[3], 5)
+        self.assertEqual(optimal_discharge_power[3], 10)
+        self.assertAlmostEqual(optimal_capacity[4], 0)
+        self.assertEqual(optimal_discharge_power[4], 0)
+        self.assertAlmostEqual(optimal_capacity[5], 0)
+
+    def test_calculating_capacity_based_on_charge_power(self):
+        solver = pywraplp.Solver('TEST SOLVER', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+        charge_power, discharge_power = add_power_schedules_to_solver(
+            solver=solver,
+            schedule_length=3,
+            max_power_kw=10,
+        )
+        capacity, cycles = add_capacity_and_cycles_to_solver(
+            solver=solver,
+            charge_power=charge_power,
+            discharge_power=discharge_power,
+            initial_battery_capacity_kwh=0,
+            final_battery_capacity_kwh=20,
+            charge_efficiency=1.0
+        )
+
+        sum_capacity = sum(capacity)
+        solver.Maximize(sum_capacity)
+        solver.Solve()
+
+        optimal_capacity = [capacity[i].solution_value() for i in range(0, len(capacity))]
+        optimal_charge_power = [charge_power[i].solution_value() for i in range(0, len(charge_power))]
+        self.assertEqual(optimal_capacity[0], 0)
+        self.assertEqual(optimal_charge_power[0], 10)
+        self.assertEqual(optimal_capacity[1], 10)
+        self.assertEqual(optimal_charge_power[1], 10)
+        self.assertEqual(optimal_capacity[2], 20)
+        self.assertEqual(optimal_charge_power[2], 0)
+        self.assertEqual(optimal_capacity[3], 20)
+
+    def test_calculating_capacity_based_on_charge_power_rte(self):
+        solver = pywraplp.Solver('TEST SOLVER', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+        charge_power, discharge_power = add_power_schedules_to_solver(
+            solver=solver,
+            schedule_length=5,
+            max_power_kw=10,
+        )
+        capacity, cycles = add_capacity_and_cycles_to_solver(
+            solver=solver,
+            charge_power=charge_power,
+            discharge_power=discharge_power,
+            initial_battery_capacity_kwh=0,
+            final_battery_capacity_kwh=20,
+            charge_efficiency=0.5
+        )
+
+        sum_capacity = sum(capacity)
+        solver.Maximize(sum_capacity)
+        solver.Solve()
+
+        optimal_capacity = [capacity[i].solution_value() for i in range(0, len(capacity))]
+        optimal_charge_power = [charge_power[i].solution_value() for i in range(0, len(charge_power))]
+        self.assertEqual(optimal_capacity[0], 0)
+        self.assertEqual(optimal_charge_power[0], 10)
+        self.assertAlmostEqual(optimal_capacity[1], 5)
+        self.assertEqual(optimal_charge_power[1], 10)
+        self.assertAlmostEqual(optimal_capacity[2], 10)
+        self.assertEqual(optimal_charge_power[2], 10)
+        self.assertAlmostEqual(optimal_capacity[3], 15)
+        self.assertEqual(optimal_charge_power[3], 10)
+        self.assertAlmostEqual(optimal_capacity[4], 20)
+        self.assertEqual(optimal_charge_power[4], 0)
+        self.assertAlmostEqual(optimal_capacity[5], 20)
+
 
 if __name__ == '__main__':
     unittest.main()
