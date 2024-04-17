45d1176c88f39628b5cf1232fe7dc4954f648a7f
Jip Rietveld <jip_rietveld@hotmail.com>
2024-04-17 16:20:25 +0200

    [NORM] Pycodestyle

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ import streamlit as st
 GIGA_HOME_PAGE = "https://giga-storage.com/en/"
 REPOSITORY_HOME_PAGE = "https://github.com/GigaStorage/battery-trading-benchmark"
 
+
 def menu():
     st.sidebar.page_link("main.py", label="Battery Trading Benchmark")
     st.sidebar.write("## Blog Posts")
